0760567304a2df28d392c966e8ece47364ffe950
Amayas Said Amer <saidameramayas@gmail.com>
2019-11-11 17:28:06 -0500

    Sketch authentication

--- final/server/web.js
+++ final/server/web.js
@@ -1,14 +1,37 @@
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 
 const PORT = 80;
 const HOSTNAME = "localhost";
 
-const fileToServe = "./www.html";
+/**
+ * These will be replaced with their hash
+ */
+const pPassword = "patient123";
+const vPassword = "volunteer123";
+
+const homePage = "./home.html";
+const patientLogin = "./patientAuth.html";
+const patientPage = "./patient.html";
+const volunteerLogin = "./volunteerAuth.html";
+const volunteerPage = "./volunteer.html";
 const pageNotFound = "./404.html";
 
 function fileToString(path) {
     return fs.readFileSync(path).toString();
+}
+
+function getQueryParams(url) {
+    return querystring.parse(new URL('http://' + HOSTNAME + url).search.slice(1));
+}
+
+function authenticatePatient(passwordHash) {
+    return passwordHash === pPassword;
+}
+
+function authenticateVolunteer(passwordHash) {
+    return passwordHash === vPassword;
 }
 
 const server = http.createServer((req, res) => {
@@ -19,12 +42,41 @@ const server = http.createServer((req, res) => {
 
     res.setHeader('Content-Type', 'text/html');
 
-    if (req.url === "/") {
-        res.statusCode = 200;
-        res.write(fileToString(fileToServe));
-    } else {
-        res.statusCode = 404;
-        res.write(fileToString(pageNotFound));
+    switch (req.url.split('?')[0]) {
+        case "/":
+            res.statusCode = 200;
+            res.write(fileToString(homePage));
+            break;
+        case "/patient":
+            res.statusCode = 200;
+            res.write(fileToString(patientLogin));
+            break;
+        case "/volunteer":
+            res.statusCode = 200;
+            res.write(fileToString(volunteerLogin));
+            break;
+        case "/vauth":
+            if (authenticateVolunteer(getQueryParams(req.url).p)) {
+                res.statusCode = 200;
+                res.write(fileToString(volunteerPage));
+            } else {
+                res.statusCode = 200;
+                res.write(fileToString(volunteerLogin));
+            }
+            break;
+        case "/pauth":
+            if (authenticatePatient(getQueryParams(req.url).p)) {
+                res.statusCode = 200;
+                res.write(fileToString(patientPage));
+            } else {
+                res.statusCode = 200;
+                res.write(fileToString(patientLogin));
+            }
+            break;
+        default:
+            res.statusCode = 404;
+            res.write(fileToString(pageNotFound));
+            break;
     }
 
     res.end();
